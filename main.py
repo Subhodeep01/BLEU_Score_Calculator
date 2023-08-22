4dd19341d980c66126acfacdba13b74db506c25e
Subhodeep01 <ghoshdeep01@gmail.con>
2023-08-22 21:30:19 +0530

    Second commit. Added recall-score and combined it with precision-score to get the f1-score and use it to calculate the modified bleu-score

--- main.py
+++ main.py
@@ -1,9 +1,30 @@
 import math
 import numpy as np
 from collections import Counter
+import copy
+
+
+def calc_prec_or_rec(a, b):
+    """
+    if a == generated text and b == reference text
+    then precision will be returned
+    otherwise
+    recall will be returned
+    """
+    r = sum(a.values())
+    for k in a:
+        if k in b:
+            if a[k] > b[k]:
+                a[k] = b[k]
+        else:
+            a[k] = 0
+    score = sum(a.values()) / r
+    return score
 
 
 def upto_n(clipped_prec):
+    if not np.nonzero(clipped_prec)[0].size > 0:
+        return [0], 1
     final_prec = [x for x in clipped_prec if x != 0]
     len_fin = len(final_prec)
     return final_prec, max(len_fin, 1)
@@ -32,9 +53,10 @@ def generate_ngram(text, n=2, n_gram=False):
     return word_list
 
 
-def bleu_score(reference, generated):
+def mod_bleu_score(reference, generated):
     """
-    Bleu score function is given the reference, or original text, and generated/machine-translated texts.
+    Modified Bleu score function is given the reference, or original text, and generated/machine-translated texts.
+    Uses both precision and recall
     """
     gen_length = len(generated.split())
     ref_length = len(reference.split())
@@ -48,28 +70,41 @@ def bleu_score(reference, generated):
 
     # Clipped precision
     clipped_precision_score = []
+    recall_score = []
     for i in range(1, min(gen_length + 1, 5)):
         ref_n_gram = Counter(generate_ngram(reference, i))
         gen_n_gram = Counter(generate_ngram(generated, i))
-        c = sum(gen_n_gram.values())
-        for j in gen_n_gram:
-            if j in ref_n_gram:
-                if gen_n_gram[j] > ref_n_gram[j]:
-                    gen_n_gram[j] = ref_n_gram[j]
-            else:
-                gen_n_gram[j] = 0
-        clipped_precision_score.append(sum(gen_n_gram.values()) / c)
 
-    final_prec, l = upto_n(clipped_precision_score)  # fetches a list of non-zero BLEU scores and also the length
-                                                     # of that list so the weight can be calculated
+        ref_n_gram2 = copy.deepcopy(ref_n_gram)
+        gen_n_gram2 = copy.deepcopy(gen_n_gram)
+
+        # Calculating the recall
+        recall_score.append(calc_prec_or_rec(ref_n_gram2, gen_n_gram2))
+
+        # Calculating the clipped-precision
+        clipped_precision_score.append(calc_prec_or_rec(gen_n_gram, ref_n_gram))
+
+    print(recall_score, clipped_precision_score)
+    final_prec, l1 = upto_n(clipped_precision_score)  # fetches a list of non-zero BLEU scores and also the length
+    final_rec, l2 = upto_n(recall_score)  # fetches a list of non-zero BLEU scores and also the length
+    # of that list so the weight can be calculated
     # weights = [0.5] * 2  # Modifying for bleu-2, would be [0.25]*4 for bleu-4     <-----Not a useful way
     # s = (w_i * math.log(p_i) for w_i, p_i in zip(weights, clipped_precision_score))     of calculating BLEU score
     # s = BP * math.exp(math.fsum(s))
-    weight = 1 / l
-    s = BP * (math.prod(final_prec) ** weight)
+    wt_prec = 1 / l1
+    wt_rec = 1 / l2
+    global_avg_prec = math.prod(final_prec) ** wt_prec
+    global_avg_rec = math.prod(final_rec) ** wt_rec
+    if global_avg_rec == global_avg_prec == 0:
+        f1_score = 0
+    else:
+        f1_score = (2 * global_avg_rec * global_avg_prec) / (global_avg_rec + global_avg_prec)
+    print(global_avg_prec, global_avg_rec)
+    s = BP * f1_score
+    print(f"Original bleu-score: {BP * global_avg_prec}")
     return s
 
 
-reference = 'I go to the school'
+reference = 'I go go to the school'
 candidate = input("Type candidate translation: ")
-print(bleu_score(reference, candidate))
+print(f"Modified bleu-score: {mod_bleu_score(reference, candidate)}")
